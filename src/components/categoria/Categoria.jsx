c13aff6008b8f9067bf680423d1c03dc664c328d
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-12-29 00:22:11 -0600

    actualizacion de categoria

--- src/components/categoria/Categoria.jsx
+++ src/components/categoria/Categoria.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import "./Categoria.css";
 
 function Categoria() {
+  let data = ["ANIME", "SPRINGBOOT", "REACT", "JAVA", "FRONTEND"];
+
   return (
-    <div>
-      categorau
+    <div className="categoria_contenedor">
+      <form className="container">
+        <div className="mb-3">
+          <label for="exampleInputCategoria" className="form-label">
+            Categoria
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputCategoria"
+          />
+        </div>
+      </form>
+
+      <ul className="list-group container">
+        {data.map((res, index) => {
+          return (
+            <li 
+            key={index}
+            className="mb-3 pb-3 list-group-item text-uppercase d-flex justify-content-between">
+              {res}
+              <span className="text-danger fw-bold icon-link-hover pointer:hover">
+                X
+              </span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Categoria
+export default Categoria;
